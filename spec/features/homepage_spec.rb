023297791ced37c4c151f2194251f448e4595f57
Roger Camps <rogercampsny@gmail.com>
2014-05-25 19:21:39 -0400

    added bootstrap

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'user enters homepage' do
 	it 'shows index about NYC' do
-		visit rooth_path
-		expect(page).to have_content('NYC')
+		visit homepage_path
+		expect(page).to have_content('NEW YORK CITY')
 	end
 end
