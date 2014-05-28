895d68e37feec2ad2f695e13e8f3f1fccaab1651
Roger Camps <rogercampsny@gmail.com>
2014-05-28 17:38:14 -0400

    add twitter bootstrap and navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap/bootstrap
 //= require_tree .
 
 	function parallax() {
---
+++ app/assets/javascripts/bootsrap.js
@@ -0,0 +1,18 @@
+jQuery ->
+  $("a[rel=popover]").popover()
+  $(".tooltip").tooltip()
+  $("a[rel=tooltip]").tooltip()
+
+
+  //=require ./affix.js
+  //=require ./button.js
+  //=require ./dropdown.js
+  //=require ./scrollspy.js
+  //=require ./transition.js
+  //=require ./alert.js
+  //=require ./carousel.js
+  //=require ./modal.js
+  //=require ./tab.js
+  //=require ./collapse.js
+  //=require ./popover.js
+  //=require ./tooltip.js
